342481c2bf18a947b0c39b934f50560d30a54724
Ayesh Jayasekara <ejkpac@gmail.com>
2017-04-04 22:54:05 +0530

    Stage 1
    First Functional Android App

--- Home/app/src/main/java/com/example/ayeshjayasekara1/home/Landing.java
+++ Home/app/src/main/java/com/example/ayeshjayasekara1/home/Landing.java
@@ -4,17 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.pubnub.api.*;
+import com.pubnub.api.callbacks.PNCallback;
+import com.pubnub.api.models.consumer.PNPublishResult;
+import com.pubnub.api.models.consumer.PNStatus;
+import java.util.Arrays;
+//import com.pubnub.*;
 
-public class Landing extends AppCompatActivity implements View.OnClickListener {
+
+
+
+public class Landing extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        PNConfiguration pnConfiguration = new PNConfiguration();
+        pnConfiguration.setSubscribeKey("sub-c-764a7b9e-16a3-11e7-bb8a-0619f8945a4f");
+        pnConfiguration.setPublishKey("pub-c-784c01b6-34fa-46e2-aba7-5eebb8efac0e");
+        pnConfiguration.setSecure(false);
+
+
+        final PubNub pubnub = new PubNub(pnConfiguration);
+
         Button btnSum =(Button)findViewById(R.id.button);
         final TextView txt = (TextView)findViewById(R.id.text100);
+        final EditText txtBox = (EditText)findViewById(R.id.editText);
         btnSum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,15 +45,30 @@ public class Landing extends AppCompatActivity implements View.OnClickListener {
 
                 Toast.makeText(getApplicationContext(),String.valueOf("33"),Toast.LENGTH_LONG).show();
                 txt.setText("Heloo Machan!!!!");
+
+
+
+
+
+
+                pubnub.publish()
+                        .message(txtBox.getText().toString())
+                        .channel("kozi")
+                        .async(new PNCallback<PNPublishResult>() {
+                            @Override
+                            public void onResponse(PNPublishResult result, PNStatus status) {
+                                // handle publish result, status always present, result if successful
+                                // status.isError to see if error happened
+                            }
+                        });
             }
         });
+
+
+
+
     }
 
 
 
-
-    @Override
-    public void onClick(View v) {
-
-    }
 }
