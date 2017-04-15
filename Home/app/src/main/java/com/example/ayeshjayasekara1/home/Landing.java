d8fba6bead8c8e6e9583fdc1641020dcc33766e8
Ayesh Jayasekara <ejkpac@gmail.com>
2017-04-15 16:30:23 +0530

    New Class
    A class with simplified controls added of portability and
    maintainability

--- Home/app/src/main/java/com/example/ayeshjayasekara1/home/Landing.java
+++ Home/app/src/main/java/com/example/ayeshjayasekara1/home/Landing.java
@@ -12,7 +12,7 @@ import com.pubnub.api.callbacks.PNCallback;
 import com.pubnub.api.models.consumer.PNPublishResult;
 import com.pubnub.api.models.consumer.PNStatus;
 
-import org.json.JSONException;
+import org.json.*;
 import org.json.JSONObject;
 import java.util.Arrays;
 //import com.pubnub.*;
@@ -27,16 +27,19 @@ public class Landing extends AppCompatActivity{
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
-        PNConfiguration pnConfiguration = new PNConfiguration();
-        pnConfiguration.setSubscribeKey("sub-c-764a7b9e-16a3-11e7-bb8a-0619f8945a4f");
-        pnConfiguration.setPublishKey("pub-c-784c01b6-34fa-46e2-aba7-5eebb8efac0e");
-        pnConfiguration.setSecure(false);
+        //PNConfiguration pnConfiguration = new PNConfiguration();
+        //pnConfiguration.setSubscribeKey("sub-c-764a7b9e-16a3-11e7-bb8a-0619f8945a4f");
+        //pnConfiguration.setPublishKey("pub-c-784c01b6-34fa-46e2-aba7-5eebb8efac0e");
+       // pnConfiguration.setSecure(false);
+        final HomeController Control = new HomeController("pub-c-784c01b6-34fa-46e2-aba7-5eebb8efac0e","sub-c-764a7b9e-16a3-11e7-bb8a-0619f8945a4f","kozi",24);
 
 
-        final PubNub pubnub = new PubNub(pnConfiguration);
+        //final PubNub pubnub = new PubNub(pnConfiguration);
 
         Button btnSum =(Button)findViewById(R.id.button);
         Button ONButton =(Button)findViewById(R.id.ON);
+        Button Con =(Button)findViewById(R.id.Conn);
+        Button T =(Button)findViewById(R.id.Test);
         Button OFFButton =(Button)findViewById(R.id.OFF);
         final TextView txt = (TextView)findViewById(R.id.text100);
         final EditText txtBox = (EditText)findViewById(R.id.editText);
@@ -52,30 +55,39 @@ public class Landing extends AppCompatActivity{
 
                 //Toast.makeText(getApplicationContext(),String.valueOf("33"),Toast.LENGTH_LONG).show();
                 txt.setText("Heloo Machan!!!!");
+                JSONObject jsonObject = null;
                 //setContentView(R.layout.activity_landing);
+                //String str = "{\"Value\":[\"1\",\"2\",\"3\",\"4\",\"5\",\"6\",\"7\",\"8\",\"9\",\"10\",\"11\"]}";
+                //String str = "{\"Value\":[\"1\""+ txtBox.getText().toString()+"]}";
+                //String str = "\"Value\":[\""+txtBox.getText().toString()+"\"]\r";
+                //[["\"Value\":\"Name"],"14921347497386015"]
+//[["\"Value\":\"Name"],"14921347497386015"]
+               // JSONArray arr = new JSONArray();
+                String str = "Value:["+txtBox.getText().toString()+"]\r";
+                try {
+                    jsonObject = new JSONObject();
+                    jsonObject.put("Value",txtBox.getText().toString());
+                    //arr.put(0,txtBox.getText().toString());
+
+                }
+                catch(JSONException e){
+                    System.out.println(e.toString());
+                }
+                System.out.println(jsonObject);
 
 
-                pubnub.publish()
-                        .message(txtBox.getText().toString())
-                        .channel("kozi")
-                        .async(new PNCallback<PNPublishResult>() {
-                            @Override
-                            public void onResponse(PNPublishResult result, PNStatus status) {
+                //pubnub.publish()
+                        //.message(jsonObject.toString().substring(1,jsonObject.toString().length()-2))
+                      //  .message(txtBox.getText().toString())
+                      //  .channel("kozi")
+                      //  .async(new PNCallback<PNPublishResult>() {
+                         //   @Override
+                        //    public void onResponse(PNPublishResult result, PNStatus status) {
                                 // handle publish result, status always present, result if successful
                                 // status.isError to see if error happened
-                            }
-                        });
+                           // }
+                        //});
 
-                pubnub.publish()
-                        .message(txtBox.getText().toString())
-                        .channel("kozi")
-                        .async(new PNCallback<PNPublishResult>() {
-                            @Override
-                            public void onResponse(PNPublishResult result, PNStatus status) {
-                                // handle publish result, status always present, result if successful
-                                // status.isError to see if error happened
-                            }
-                        });
             }
         });
 
@@ -98,16 +110,16 @@ public class Landing extends AppCompatActivity{
 
 
 
-                pubnub.publish()
-                        .message("000000000000000")
-                        .channel("kozi")
-                        .async(new PNCallback<PNPublishResult>() {
-                            @Override
-                            public void onResponse(PNPublishResult result, PNStatus status) {
+               // pubnub.publish()
+                       // .message("000000000000000")
+                       // .channel("kozi")
+                        //.async(new PNCallback<PNPublishResult>() {
+                        //    @Override
+                       //     public void onResponse(PNPublishResult result, PNStatus status) {
                                 // handle publish result, status always present, result if successful
                                 // status.isError to see if error happened
-                            }
-                        });
+                      //      }
+                     //   });
             }
         });
 
@@ -124,16 +136,30 @@ public class Landing extends AppCompatActivity{
 
 
 
-                pubnub.publish()
-                        .message("11111111111111")
-                        .channel("kozi")
-                        .async(new PNCallback<PNPublishResult>() {
-                            @Override
-                            public void onResponse(PNPublishResult result, PNStatus status) {
-                                // handle publish result, status always present, result if successful
+               // pubnub.publish()
+                     //   .message("11111111111111")
+                    //    .channel("kozi")
+                   //     .async(new PNCallback<PNPublishResult>() {
+                         //   @Override
+                      //      public void onResponse(PNPublishResult result, PNStatus status) {
+                   //             // handle publish result, status always present, result if successful
                                 // status.isError to see if error happened
-                            }
-                        });
+                       //     }
+                   //    });
+            }
+        });
+
+        Con.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                    Control.setAllOn();
+            }
+        });
+
+        T.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Control.setAllOff();
             }
         });
 
